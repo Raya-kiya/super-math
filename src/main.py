8a1c2fddc8edca397312ecdde10c72da4ada0e20
Raya-kiya <ra.khass3005@gmail.com>
2023-09-13 23:09:17 +0300

    gfdhg

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import modules.factorial as factorial
 import modules.test as t
 import modules.tests.test_all as ta
 
+print("start")
 f1 = factorial.factorial_rec(5)
 print(f1)
 
@@ -23,3 +24,4 @@ print(p1)
 p2 = perimeter.perimeter_func([1, 2, 5, 7, 3])
 print(p2)
 print("hello world")
+print("end")
